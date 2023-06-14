526322be21cee012fd90fd8a28cba284c7f55172
hellocyu <hellocyu.163.com>
2023-06-14 16:54:58 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div>我是APP根组件</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+</script>
 
 <style scoped></style>
