d6e59739bdcfe85ac565687accd8e733e8ef4036
hellocyu <hellocyu.163.com>
2023-06-14 17:19:32 +0800

    feat: add

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
   <div>我是APP根组件</div>
 </template>
 
-<script setup lang="ts">
-for (let index = 0; index < 10; index++) {
-  console.log(index);
-}
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
